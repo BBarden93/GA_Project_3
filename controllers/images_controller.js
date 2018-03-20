b2d40c082e872ab7b0d671ee1914f238824a6ca3
Robert Shuman <thegreatspirit.turtle@gmail.com>
2018-03-20 10:58:33 -0700

    Images router errors fixed

--- controllers/images_controller.js
+++ controllers/images_controller.js
@@ -10,31 +10,31 @@ module.exports = {                                                              
     show: (req, res) => {
         Image.findById(req.params.id, (err, thatimage) => {
             if(err) return console.log(err)
-            res.render("images/show", {image: thatimage})
+            res.render("show", {image: thatimage})
         })                          
     },
     new: (req, res) => {
-            res.render("images/new")
+            res.render("newImage")
     }, 
     
     create: (req, res) => {
         Image.create(req.body, (err, newimage) =>{
             if(err) return console.log(err)
-            res.render("images/show", {image: newimage})
+            res.render("show", {image: newimage})
         })
     }, 
 
     edit: (req, res) => {
         Image.findById(req.params.id, (err, thatimage) =>{  
             if(err) return console.log(err)
-            res.render("images/edit", {image: thatimage})
+            res.render("editImage", {image: thatimage})
         })                    
     }, 
 
     update: (req, res) => {
          Image.findByIdAndUpdate(req.params.id, req.body, (err, updatedimage) => {                     // Find the image by the id and update the body(two arguments)
             if(err) return console.log(err)
-            res.render("images/", {image: updatedimage}) 
+            res.render("show", {image: updatedimage}) 
          })                                                                                        
     }, 
 
