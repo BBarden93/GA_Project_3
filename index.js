c0618a2fbd0fcb678b87b523294574819390d8a4
Brittany Barden <bbarden93@yahoo.com>
2018-03-21 14:35:55 -0700

    mongo db uri environment variable

--- index.js
+++ index.js
@@ -18,10 +18,13 @@ const imagesRouter = require('./routes/images.js')
 const Image = require('./models/Image.js')
 const httpClient = axios.create()
 const methodOverride = require('method-override')
-const PORT = 3000
+
 require('dotenv').config()
 const apiKey = process.env.API_KEY
 
+const 
+	port = process.env.PORT || 3000, 
+	mongoConnectionString = process.env.MONGODB_URI || 'mongodb://localhost/vacation-finder'
 //MONGOOSE CONNECT
 mongoose.connect ("mongodb://localhost/vacation-finder", (err) => {
     console.log(err || "Connected to MongoDB")
@@ -74,6 +77,6 @@ app.use('/users', usersRouter)
 app.use('/images', imagesRouter)
 
 
-app.listen(PORT, (err) => {
-	console.log(err || `Listening on ${PORT}`)
+app.listen(port, (err) => {
+	console.log(err || `Listening on ${port}`)
 })
